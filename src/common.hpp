6d7a951017a274e093706fcf203b89beef980f8b
elmerucr <elmerucr@gmail.com>
2024-03-10 10:25:11 +0100

    date stamp

--- src/common.hpp
+++ src/common.hpp
@@ -13,7 +13,7 @@
  */
 #define PUNCH_MAJOR_VERSION    0
 #define PUNCH_MINOR_VERSION    6
-#define PUNCH_BUILD            20240306
+#define PUNCH_BUILD            20240307
 #define PUNCH_YEAR             2024
 
 /*
